25a1438c45dcc19157c7a007fe57a3bf8fb8ac8e
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-13 23:00:31 +0530

    Model layer for the Task Manager

---
+++ src/main/java/com/example/taskManager/model/Note.java
@@ -0,0 +1,8 @@
+package com.example.taskManager.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Note {
+    
+}
---
+++ src/main/java/com/example/taskManager/model/Task.java
@@ -0,0 +1,9 @@
+package com.example.taskManager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity @Table (name="tasks")
+public class Task {
+    
+}
