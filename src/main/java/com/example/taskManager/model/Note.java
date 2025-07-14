880688ae081e846c2c33edde57f61f684523c8e1
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-14 17:43:51 +0530

    POJO classes for the TaskManager

--- src/main/java/com/example/taskManager/model/Note.java
+++ src/main/java/com/example/taskManager/model/Note.java
@@ -1,8 +1,40 @@
 package com.example.taskManager.model;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
+@Entity @Table(name = "notes") @Getter @Setter
 public class Note {
     
+    //Fields
+    
+    @Id @GeneratedValue
+    private int notesId;
+
+    @ManyToOne @JoinColumn(name="taskId")
+    private Task task;
+
+    private String content;
+    
+    @Column(columnDefinition = "DATETIME2")
+    private LocalDateTime createdAt;
+    
+    //empty constructor
+    public Note(){}
+
+    //constructor
+    public Note(int notesId, Task task, String content, LocalDateTime createdAt){
+        this.notesId = notesId;
+        this.task = task;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/example/taskManager/model/Task.java
+++ src/main/java/com/example/taskManager/model/Task.java
@@ -1,9 +1,44 @@
 package com.example.taskManager.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity @Table (name="tasks")
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity @Table (name="tasks") @Getter @Setter
 public class Task {
     
+    //fields
+
+    @Id @GeneratedValue
+    private int taskId;
+
+    private String title;
+    private String description;
+    
+    @Column(columnDefinition = "DATETIME2")
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Note> notes = new ArrayList<>();
+
+    //empty constructor
+    public Task(){}
+
+    //constructor
+    public Task(int taskId, String title, String description, LocalDateTime createdAt){
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
 }
