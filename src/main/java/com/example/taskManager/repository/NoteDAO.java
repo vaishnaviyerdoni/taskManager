c2b2ba1e3e761c122b0f432c9265061a2732b0cf
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-15 00:20:20 +0530

    working on the dao methods for task Manager

--- src/main/java/com/example/taskManager/repository/NoteDAO.java
+++ src/main/java/com/example/taskManager/repository/NoteDAO.java
@@ -1,9 +1,32 @@
 package com.example.taskManager.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.taskManager.model.Task;
+import java.util.ArrayList;
+import java.util.List;
 
-public interface NoteDAO extends JpaRepository<Task, Integer>{
+import org.springframework.stereotype.Repository;
 
-    
+import com.example.taskManager.model.Note;
+
+@Repository
+public class NoteDAO{
+
+    /* 
+    private Note note;
+
+    //constructor
+    public NoteDAO(Note note){
+        this.note = note;
+    }
+        */
+
+    //to get all notes by taskId
+    public List<Note> getNotebyTasId(int TaskId) {
+
+        List<Note> notes = new ArrayList<>();
+        try{
+
+        }
+        catch(Exception e){}
+        return notes;
+    }
 }
--- src/main/java/com/example/taskManager/repository/TaskDAO.java
+++ src/main/java/com/example/taskManager/repository/TaskDAO.java
@@ -1,9 +1,7 @@
 package com.example.taskManager.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.taskManager.model.Task;
 
-public interface TaskDAO extends JpaRepository<Task, Integer> {
+public class TaskDAO {
 
     
 } 
