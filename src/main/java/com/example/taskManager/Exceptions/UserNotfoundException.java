ca330b8ed865b6c5a08e2bda65af8315982985d3
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-16 17:59:49 +0530

    Custom exception

---
+++ src/main/java/com/example/taskManager/Exceptions/UserNotfoundException.java
@@ -0,0 +1,7 @@
+package com.example.taskManager.Exceptions;
+
+public class UserNotfoundException extends Exception {
+    public UserNotfoundException(String message){
+        super(message);
+    }
+}
