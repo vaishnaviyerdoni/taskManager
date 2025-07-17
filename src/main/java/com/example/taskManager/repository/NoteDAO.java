34bed4e5941a3e582a04df6da8c16d1ba58a025f
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-17 17:42:37 +0530

    updating dao for crud

--- src/main/java/com/example/taskManager/repository/NoteDAO.java
+++ src/main/java/com/example/taskManager/repository/NoteDAO.java
@@ -3,24 +3,22 @@ package com.example.taskManager.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
 import com.example.taskManager.model.Note;
 
 @Repository
 public class NoteDAO{
-
-    /* 
-    private Note note;
+    private JdbcTemplate jdbc;
 
     //constructor
-    public NoteDAO(Note note){
-        this.note = note;
+    public NoteDAO(JdbcTemplate jdbc){
+        this.jdbc = jdbc;
     }
-        */
+        
 
     //to get all notes by taskId
-    public List<Note> getNotebyTasId(int TaskId) {
+    public List<Note> getNotebyTaskId(int TaskId) {
 
         List<Note> notes = new ArrayList<>();
         try{
