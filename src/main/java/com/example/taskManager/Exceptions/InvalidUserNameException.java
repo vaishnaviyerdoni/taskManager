4046c0b8c3b08c1f9570395851190144ae6ab74f
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-18 22:44:16 +0530

    Custom Exeptions

---
+++ src/main/java/com/example/taskManager/Exceptions/InvalidUserNameException.java
@@ -0,0 +1,7 @@
+package com.example.taskManager.Exceptions;
+
+public class InvalidUserNameException extends Exception {
+    public InvalidUserNameException(String message){
+        super(message);
+    }
+}
