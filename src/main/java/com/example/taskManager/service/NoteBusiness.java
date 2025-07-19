b8c37368dc59c0c9fcba3cd01e17a86cb4a85f28
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-19 16:42:58 +0530

    working on service layer

--- src/main/java/com/example/taskManager/service/NoteBusiness.java
+++ src/main/java/com/example/taskManager/service/NoteBusiness.java
@@ -1,8 +1,23 @@
 package com.example.taskManager.service;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.slf4j.*;
 
 @Service
 public class NoteBusiness {
-    
+    private static final Logger logger = LoggerFactory.getLogger(NoteBusiness.class);
+    private JdbcTemplate jdbc;
+
+    public NoteBusiness(JdbcTemplate jdbc){
+        this.jdbc = jdbc;
+    }
+
+    //Add a note to task
+
+    //for a given task, get all its notes
+
+    //update the note for that taskId
+
+    //delete a note for the task
 }
--- src/main/java/com/example/taskManager/service/TaskBusiness.java
+++ src/main/java/com/example/taskManager/service/TaskBusiness.java
@@ -1,8 +1,24 @@
 package com.example.taskManager.service;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.slf4j.*;
 
 @Service
 public class TaskBusiness {
-    
+    private static final Logger logger = LoggerFactory.getLogger(TaskBusiness.class);
+    private JdbcTemplate jdbc;
+
+    public TaskBusiness(JdbcTemplate jdbc){
+        this.jdbc = jdbc;
+    }
+
+    //Add Task to Task manager
+
+    //Get all tasks
+
+    //update the task Content(task Content is actual task body)
+
+    //Delete the task
+
 }
