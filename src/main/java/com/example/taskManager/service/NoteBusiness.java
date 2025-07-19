78d5c964cca23eb7a9ef05b46ef8c8183e01d2a1
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-19 21:13:12 +0530

    working on business layer

--- src/main/java/com/example/taskManager/service/NoteBusiness.java
+++ src/main/java/com/example/taskManager/service/NoteBusiness.java
@@ -1,19 +1,56 @@
 package com.example.taskManager.service;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import com.example.taskManager.Exceptions.UserNotfoundException;
+import com.example.taskManager.model.Note;
+import com.example.taskManager.model.Task;
+import com.example.taskManager.model.User;
+import com.example.taskManager.repository.NoteDAO;
+import com.example.taskManager.repository.TaskDAO;
+import com.example.taskManager.repository.UserDAO;
+import java.time.LocalDateTime;
 import org.slf4j.*;
 
 @Service
 public class NoteBusiness {
     private static final Logger logger = LoggerFactory.getLogger(NoteBusiness.class);
-    private JdbcTemplate jdbc;
+    TaskDAO taskDAO;
+    UserDAO userDAO;
+    NoteDAO noteDAO;
 
-    public NoteBusiness(JdbcTemplate jdbc){
-        this.jdbc = jdbc;
+    public NoteBusiness(TaskDAO taskDAO, UserDAO userDAO, NoteDAO noteDAO){
+        this.userDAO = userDAO;
+        this.taskDAO = taskDAO;
+        this.noteDAO = noteDAO;
     }
 
     //Add a note to task
+    public int addNoteMytask(int notesId, int taskId, int userId, String content, LocalDateTime createdAt, String userName) {
+        try{
+            int userID = userDAO.getUserIdbyUsername(userName);
+            int taskID = taskDAO.getTaskIdbyuserId(userID);
+
+            if(taskId == taskID && userID == userId) {
+                Task task = new Task();
+                task.setTaskId(taskID);
+
+                User user = new User();
+                user.setUserId(userID);
+
+                Note note = new Note(notesId, task, user, content, createdAt);
+                int notesID = noteDAO.addNote(taskID, note);
+                return notesId;
+            }
+            else{
+                throw new UserNotfoundException("User for the given userName not found");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            logger.error("Failed to add note", e);
+            return -1;
+        }
+    }
 
     //for a given task, get all its notes
 
--- src/main/java/com/example/taskManager/service/TaskBusiness.java
+++ src/main/java/com/example/taskManager/service/TaskBusiness.java
@@ -1,24 +1,132 @@
 package com.example.taskManager.service;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import com.example.taskManager.Exceptions.UserNotfoundException;
+import com.example.taskManager.model.Task;
+import com.example.taskManager.model.User;
+import com.example.taskManager.repository.TaskDAO;
+import com.example.taskManager.repository.UserDAO;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.*;
 import org.slf4j.*;
 
 @Service
 public class TaskBusiness {
     private static final Logger logger = LoggerFactory.getLogger(TaskBusiness.class);
-    private JdbcTemplate jdbc;
+    private UserDAO userDAO;
+    private TaskDAO taskDAO;
 
-    public TaskBusiness(JdbcTemplate jdbc){
-        this.jdbc = jdbc;
+    public TaskBusiness(UserDAO userDAO, TaskDAO taskDAO){
+        this.userDAO = userDAO;
+        this.taskDAO = taskDAO;
     }
 
     //Add Task to Task manager
+    public int addTasktoManager(int taskId, int userId, String title, String taskContent, LocalDateTime createdAt, String userName) {
+        int userID = 0;
+        int taskID = 0;
+        try{
+            userID = userDAO.getUserIdbyUsername(userName);
+            User user = new User();
+            if(userID == userId){
+                user.setUserId(userID);
+                Task task = new Task(taskID, user, title, taskContent, createdAt);
+                taskID = taskDAO.addTask(task);
+                return taskID;
+            }
+            else{
+                throw new UserNotfoundException("user for given username not found!");
+            }  
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            logger.error("Could not add Task", e);
+            return -1;
+        }
+    }
 
     //Get all tasks
+    public List<Task> getMyTasks(int userId, int taskId, String userName) {
+        List<Task> tasks = new ArrayList<>();
+        try{
+            int userID = userDAO.getUserIdbyUsername(userName);
+            if(userId == userID){
+                tasks = taskDAO.getAllTasks(userID);
+                return tasks;
+            }
+            else{
+                throw new UserNotfoundException("User for given userID not found");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            logger.error("Error while fetching task information", e);
+            return Collections.emptyList();
+        }
+    }
+
+    public List<Task> getTaskbyId(int taskId, int userId, String userName){
+        List<Task> tasks = new ArrayList<>();
+        try{
+            int userID = userDAO.getUserIdbyUsername(userName);
+            int taskID = taskDAO.getTaskIdbyuserId(userID);
+
+            if(taskID == taskId && userID == userId){
+                tasks = taskDAO.getTaskbyID(taskID);
+                return tasks;
+            }
+            else{
+                throw new UserNotfoundException("User for the given userID not found");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            logger.error("User for given userName not found", e);
+            return Collections.emptyList();
+        }
+    }
 
     //update the task Content(task Content is actual task body)
+    public boolean updateTask(String newContent, int taskId, int userId, String userName){
+        try{
+            int userID = userDAO.getUserIdbyUsername(userName);
+            int taskID = taskDAO.getTaskIdbyuserId(userID);
+
+            if(taskId == taskID && userID == userId){
+                boolean isUpdated = taskDAO.updateTask(newContent, taskID);
+                return isUpdated;
+            }
+            else{
+                throw new UserNotfoundException("User for the given userID not found");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            logger.error("Error while updating task", e);
+            return false;
+        }
+    }
 
     //Delete the task
+    public boolean deleteMytask(int taskId, int userId, String userName){
+        try{
+            int userID = userDAO.getUserIdbyUsername(userName);
+            int taskID = taskDAO.getTaskIdbyuserId(userID);
+
+            if(taskId == taskID && userID == userId){
+                boolean isDeleted = taskDAO.deleteTask(taskID);
+                return isDeleted;
+            }
+            else{
+                throw new UserNotfoundException("User for the given userID not found");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            logger.error("Error while deleting task", e);
+            return false;
+        }
+    }
 
 }
