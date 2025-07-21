2169a5d06c28701ceed7b58b75e05f3df3788b71
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-21 21:58:22 +0530

    controller ayer for task manager

--- src/main/java/com/example/taskManager/controller/UserController.java
+++ src/main/java/com/example/taskManager/controller/UserController.java
@@ -6,6 +6,9 @@ import java.util.*;
 import com.example.taskManager.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -27,4 +30,11 @@ public class UserController {
             return Collections.emptyList();
         }
     }
+
+    @PostMapping("/user/{}")
+    public String postMethodName(@RequestBody String entity) {
+        
+        return entity;
+    }
+    
 }
