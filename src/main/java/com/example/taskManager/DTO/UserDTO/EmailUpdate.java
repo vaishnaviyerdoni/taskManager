0dfc3f63d66651aa1e4baa43b9e33f7bc39d26ef
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-22 18:10:11 +0530

    Data Transfer Object

---
+++ src/main/java/com/example/taskManager/DTO/UserDTO/EmailUpdate.java
@@ -0,0 +1,16 @@
+package com.example.taskManager.DTO.UserDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class EmailUpdate {
+   private String userName;
+   private String email; 
+}
