7bc24adf52840e896b3c3dff3ba3e93ad4dc3f4a
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-22 19:48:49 +0530

    Controller layer for user management

--- src/main/java/com/example/taskManager/controller/UserController.java
+++ src/main/java/com/example/taskManager/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @RestController
 @RequestMapping("/api")
 public class UserController {
