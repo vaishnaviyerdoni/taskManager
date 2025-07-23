a1ebadb38aadf314612613ffac8e662e3d1c3d67
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-23 19:23:51 +0530

    controller layer for task manager

--- src/main/java/com/example/taskManager/controller/NoteController.java
+++ src/main/java/com/example/taskManager/controller/NoteController.java
@@ -1,8 +1,12 @@
 package com.example.taskManager.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RequestMapping("/api")
 @RestController
+
 public class NoteController {
     
 }
