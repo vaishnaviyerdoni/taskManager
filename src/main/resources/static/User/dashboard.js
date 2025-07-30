0699858c9308399e4cac98bc1a19248a12b437e4
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-07-30 22:47:29 +0530

    JavaScript for taskManager

---
+++ src/main/resources/static/User/dashboard.js
@@ -0,0 +1,9 @@
+console.log("dashboard.js is loading");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM is loading");
+
+    document.getElementById("logout").addEventListener("click", () => {
+        localStorage.clear();
+        window.location.href = "logout.html";
+    });
+})
