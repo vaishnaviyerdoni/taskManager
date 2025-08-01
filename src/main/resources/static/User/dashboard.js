69f7c1015e9789453e99065464aba8efe03f0672
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-08-02 02:12:10 +0530

    minor changes in dashboard

--- src/main/resources/static/User/dashboard.js
+++ src/main/resources/static/User/dashboard.js
@@ -2,6 +2,10 @@ console.log("dashboard.js is loading");
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM is loading");
 
+    document.getElementById("gotoTasksBtn").addEventListener("click", () => {
+        window.location.href = "tasks.html";
+    })
+
     document.getElementById("logout").addEventListener("click", () => {
         localStorage.clear();
         window.location.href = "logout.html";
