2f30e3c43933c88db85e238b123a5c03141b6556
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-08-04 00:33:47 +0530

    minor correction

--- src/main/resources/static/User/dashboard.js
+++ src/main/resources/static/User/dashboard.js
@@ -2,12 +2,12 @@ console.log("dashboard.js is loading");
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM is loading");
 
-    document.getElementById("gotoTasksBtn").addEventListener("click", () => {
-        window.location.href = "tasks.html";
-    })
-
     document.getElementById("logout").addEventListener("click", () => {
         localStorage.clear();
         window.location.href = "logout.html";
     });
+
+    document.getElementById("gotoTasksBtn").addEventListener("click", () => {
+        window.location.href = "tasks.html";
+    })
 })
