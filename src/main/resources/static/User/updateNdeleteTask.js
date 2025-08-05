6af84405c424194e6ce56faae0d42b1289d7b9d9
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-08-05 21:33:37 +0530

    Added delete task logic

--- src/main/resources/static/User/updateNdeleteTask.js
+++ src/main/resources/static/User/updateNdeleteTask.js
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("taskId").value = taskID;
     }
 
+    //update task logic
     const userNamefromStorage = localStorage.getItem("userName");
     if(userNamefromStorage){
         document.getElementById("userNameforUpdate").value = userNamefromStorage;
@@ -68,6 +69,50 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 
+    //delete task logic
+    const userIDStorage = localStorage.getItem("userId");
+    if(userIDStorage){
+        document.getElementById("userID").value = userIDStorage;
+    }
+
+    const userNameStorage = localStorage.getItem("userName");
+    if(userNameStorage){
+        document.getElementById("userName").value = userNameStorage;
+    }
+
+    const deleteForm = document.getElementById("myDeleteForm");
+    deleteForm.addEventListener("submit", async(e) => {
+        e.preventDefault();
+
+        const userId = document.getElementById("userID").value.trim();
+        const userName = document.getElementById("userName").value.trim();
+        const taskId = document.getElementById("taskId").value.trim();
+
+        console.log("Received information: ", userId, userName, taskId);
+
+        try{
+            const res = await fetch(`/api/task/${taskId}?userId=${encodeURIComponent(userId)}&userName=${encodeURIComponent(userName)}`, {
+                method : "DELETE"
+            });
+
+            const result = await res.text();
+            if(res.ok){
+                alert(result);
+                setTimeout(() => {
+                    window.location.href = "tasks.html";
+                }, 3000)
+            }
+            else{
+                document.getElementById("confirmationMessage").innerText = result;
+            }
+        }
+        catch(error){
+            console.log("Server Response", error);
+            document.getElementById("confirmationMessage").innerText = "Server Error, try again later!";
+        }
+    })
+
+    //dashboard button logic
     const BtntoDashboard = document.getElementById("dashboard");
     if(BtntoDashboard){
         BtntoDashboard.addEventListener("click", () => {
