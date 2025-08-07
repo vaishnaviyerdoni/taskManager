01cd6f06ea3c085af4b10cce1d7a0798b172453c
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-08-08 01:21:45 +0530

    DTO

--- src/main/java/com/example/taskManager/DTO/UserDTO/PasswordUpdate.java
+++ src/main/java/com/example/taskManager/DTO/UserDTO/PasswordUpdate.java
@@ -1,9 +1,9 @@
 package com.example.taskManager.DTO.UserDTO;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Getter;
 
 @AllArgsConstructor
 @NoArgsConstructor
